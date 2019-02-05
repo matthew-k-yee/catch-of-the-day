69bc74f5b2c8ac68302428ad653d9cb710b04c5f
Matthew Yee <matthewyee@Jacky-Man.local>
2019-02-04 23:18:53 -0500

    index.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class StorePicker extends React.Component {
+  render() {
+    return (
+      <h1>hi</h1>
+    )
+  }
+}
+
+ReactDOM.render(<StorePicker />, document.querySelector('#main'));
